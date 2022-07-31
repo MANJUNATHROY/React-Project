440d1ba2726472ac879c62f6d93219ab6912086c
Ashik-Jose <ashiksholly@gmail.com>
2022-07-31 11:25:44 +0530

    Added

---
+++ Flipkart Clone/server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import userRoutes from './routes/user.js'
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+app.use(cors());
+
+app.use('/api',userRoutes);
+
+app.get('/',(req,res,next) => {
+    res.status(200).json({
+        message: "Hello"
+    });
+});
+
+const PORT = process.env.PORT || 2000;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true})
+.then(() => app.listen(PORT,() => console.log(PORT)))
+.catch((error) => console.log(error.message));
